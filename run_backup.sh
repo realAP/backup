1c2311c59a84f59addcd032908cdd09542706036
realAP <devp@posteo.de>
2024-06-16 13:22:06 +0200

    add run file with environment variables

---
+++ run_backup.sh
@@ -0,0 +1,20 @@
+#!/bin/env bash
+source .env
+docker run --restart=always --rm -it --hostname restic \
+  -v "${DATA_TO_BACKUP}":/source \
+  -v "${ENV_NAME_OF_PRIVATE_KEY}":/private_key \
+  -v ./restore:/restore \
+  -v ./log/:/var/log \
+  -e "DEBUG"=0 \
+  -e "TARGET_DOMAIN"="${ENV_TARGET_DOMAIN}" \
+  -e "TARGET_DOMAIN_USER"="${ENV_TARGET_DOMAIN_USER}" \
+  -e "PATH_OF_PRIVATE_KEY"="${ENV_PATH_OF_PRIVATE_KEY}" \
+  -e "NC_USER"="${ENV_NC_USER}" \
+  -e "NC_PASS"="${ENV_NC_PASS}" \
+  -e "NC_URL"="${ENV_NC_URL}" \
+  -e "RESTIC_REPOSITORY_NAME"="${ENV_RESTIC_REPOSITORY_NAME}" \
+  -e "RESTIC_PASSWORD"="${ENV_RESTIC_PASSWORD}" \
+  -e "TELEGRAM_TOKEN"="${ENV_TELEGRAM_TOKEN}" \
+  -e "TELEGRAM_CHAT_ID"="${ENV_TELEGRAM_CHAT_ID}" \
+  -e "CRON"='0 1 * * *' \
+  restic $@
