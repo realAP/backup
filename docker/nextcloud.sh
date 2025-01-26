06e19a164614f08a3afc000ea0cc19dd7b22bfdf
realAP <44711679+realAP@users.noreply.github.com>
2025-01-26 15:06:06 +0100

    21 logging does not show current backup process (#23)
    * run pipeline for dev brunch
    
    * some kind of better output when starting

--- docker/nextcloud.sh
+++ docker/nextcloud.sh
@@ -1,6 +1,6 @@
-#!/bin/bash -l
+#!/usr/bin/env bash
 set -e
 mkdir -p /source/nextcloud
 echo "----------- START NEXTCLOUD CLIENT -----------"
-nextcloudcmd --non-interactive --silent -u $NC_USER -p "$NC_PASS" /source/nextcloud $NC_URL
+nextcloudcmd --non-interactive --silent -u $NC_USER -p "$NC_PASS" /source/nextcloud "$NC_URL"
 echo "----------- END NEXTCLOUD CLIENT -----------"
